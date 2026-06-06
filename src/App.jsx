5242dc923abe000166869466df9081f2d2de6e71
Vansh Saini <macbook-air@Vanshs-MacBook-Air.local>
2026-06-06 20:51:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <h1 class="text-4xl font-light underline text-indigo-600">
+    Hello world!
+  </h1>
+  )
+}
+
+export default app
