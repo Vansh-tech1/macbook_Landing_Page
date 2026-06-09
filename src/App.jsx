3c0b36d625e348b368601e1f48f0f48e7b9e4b13
Vansh Saini <macbook-air@Vanshs-MacBook-Air.local>
2026-06-09 10:16:59 +0530

    implement product viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import ProductViewer from "./components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Showcase from "./components/Showcase";
 
+gsap.registerPlugin(ScrollTrigger); 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
+      <Showcase />
     </main>
   );
 };
