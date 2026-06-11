fdb98ae536149af7a6d4ea06d5fc4918ff40cbaa
Vansh Saini <macbook-air@Vanshs-MacBook-Air.local>
2026-06-11 17:06:26 +0530

    add a get in map function

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -101,7 +101,10 @@ const Features = () => {
 
       <div className="absolute inset-0">
         {features.map((feature, index) => (
-          <div className={clsx("box", `box${index + 1}`, feature.styles)}>
+          <div
+            className={clsx("box", `box${index + 1}`, feature.styles)}
+            key={feature.id}
+          >
             <img src={feature.icon} alt={feature.highlight} />
             <p>
               <span className="text-white">{feature.highlight}</span>
